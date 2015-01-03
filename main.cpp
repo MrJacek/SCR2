3fe5d5f1a3d2dbfeb5323e9cc93ac04fdbef791c
iskrzycki <iskrzycki@gmail.com>
2015-01-03 15:47:43 +0100

    Stworzenie wątku wieży w mainie

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "main.h"
 #include "Env.h"
 #include "IRobot.h"
-
+#include "Wieza.h"
 
 int main() {
     std::thread env(environmentRun);
     std::thread robot(irobotRun);
-
+	std::thread rook(wiezaRun);
     
     std::cout << "main, foo and bar now execute concurrently...\n";
 
